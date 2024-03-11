c706bc860b98350db5fddea31e75d9de7d078b4f
Jorge Montanares <98401636+Montanares@users.noreply.github.com>
2024-03-11 00:05:09 -0300

    feat(App.tsx): login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+  const handleChangeName = (event: any) => {
+    setName(event.target.value)
+  }
+  const handleChangePassword = (event: any) => {
+    setPassword(event.target.value)
+  }
+  const handleClickLogin = () => {
+    console.log('iniciar', name, password)
+  }
+
   return (
     <div className="App">
-      Hola
+      <input type="text" value={name} onChange={handleChangeName} className='form-control'/>
+      <input type="password" value={password} onChange={handleChangePassword} className='form-control'/>
+      <button onClick={handleClickLogin} className='btn btn-primary'>Iniciar Sesión</button>
     </div>
   );
 }
