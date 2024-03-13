4351b83ded6bc72196a5a31f6cb3d9ed7c24a816
Jorge Montanares <98401636+Montanares@users.noreply.github.com>
2024-03-12 23:59:39 -0300

    feat: agregar input email

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import { useState } from 'react';
 import './App.css';
+import DisplayData from './components/display-data';
 
 function App() {
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
   const handleChangeName = (event: any) => {
     setName(event.target.value)
   }
   const handleChangePassword = (event: any) => {
     setPassword(event.target.value)
   }
+  const handleChangeEmail = (event: any) => setEmail(event.target.value)
   const handleClickLogin = () => {
     console.log('iniciar', name, password)
   }
 
   return (
-    <div className="App">
-      <input type="text" value={name} onChange={handleChangeName} className='form-control'/>
-      <input type="password" value={password} onChange={handleChangePassword} className='form-control'/>
-      <button onClick={handleClickLogin} className='btn btn-primary'>Iniciar Sesión</button>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="App">
+            <input type="text" placeholder="escriba nombre" value={name} onChange={handleChangeName} className='form-control' />
+            <input type="password" placeholder='escriba contraseña' value={password} onChange={handleChangePassword} className='form-control' />
+            <input type="email" value={email} onChange={handleChangeEmail} className='form-control'/>
+            <button onClick={handleClickLogin} className='btn btn-primary'>Iniciar Sesión</button>
+          </div>
+        </div>
+      </div>
+      <DisplayData />
     </div>
   );
 }
---
+++ src/components/display-data/index.tsx
@@ -0,0 +1,7 @@
+const DisplayData = () => {
+    return (
+        <p>Hola soy un display data</p>
+    )
+}
+
+export default DisplayData
